3127bd4ed806d0a77f4210d390c6ce2ef5e8073b
Christian Joergensen <christian@technobabble.dk>
2014-07-14 13:55:41 +0200

    Refactor.

---
+++ address.go
@@ -0,0 +1,15 @@
+package smtpd
+
+import (
+	"strings"
+	"fmt"
+)
+
+type MailAddress string
+
+func parseMailAddress(src string) (MailAddress, error) {
+	if src[0] != '<' || src[len(src)-1] != '>' || strings.Count(src, "@") != 1 {
+		return MailAddress(""), fmt.Errorf("Ill-formatted e-mail address: %s", src)
+	}
+	return MailAddress(src[1 : len(src)-1]), nil
+}
