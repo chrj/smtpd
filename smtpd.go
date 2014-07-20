73d3eb24c507d38454154724da798cfc0bae031f
Christian Joergensen <christian.joergensen@gmta.info>
2014-07-20 21:53:47 +0200

    Update synopsis.

--- smtpd.go
+++ smtpd.go
@@ -1,4 +1,4 @@
-// Package smtpd implements an SMTP server with support for STARTTLS, authentication (PLAIN/LOGIN) and optional restrictions on the different stages of the SMTP session.
+// Package smtpd implements an SMTP server with support for STARTTLS, authentication (PLAIN/LOGIN), XCLIENT and optional restrictions on the different stages of the SMTP session.
 package smtpd
 
 import (
